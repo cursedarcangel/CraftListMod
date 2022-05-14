97c4f2bf122f572ccc3224d5ca88977fbd63cb90
cursedarcangel <ufdoisa@gmail.com>
2022-05-14 18:22:32 -0400

    Refactor parsing json to not use external method

--- src/main/java/com/cursedarcangel/craftlist/Craftlist.java
+++ src/main/java/com/cursedarcangel/craftlist/Craftlist.java
@@ -22,20 +22,6 @@ import static net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.lit
 public class Craftlist implements ModInitializer {
     @Override
     public void onInitialize() {
-
-        MinecraftClient mc = MinecraftClient.getInstance();
-        ClientCommandManager.DISPATCHER.register(
-                literal("craftlist")
-                        .then(argument("item", string())
-                                .then(argument("amount", integer())
-                                        .executes(ctx -> {
-                                            String item = getString(ctx, "item");
-                                            int count = getInteger(ctx, "amount");
-                                            parse(item, count);
-                                            return 1;
-                                        }))));
-    }
-    public static void parse(String item, int amount){
         Gson gson = new Gson();
         Reader reader = null;
 
@@ -49,20 +35,30 @@ public class Craftlist implements ModInitializer {
             materialsPerItem.put(String.valueOf(index.getKey()), index.getValue());
         }
 
-        Map<String, Double> mats = (Map<String, Double>) materialsPerItem.get(item);
         MinecraftClient mc = MinecraftClient.getInstance();
-        if (mats == null) {
-            String msg = "recipe not found";
-            mc.inGameHud.addChatMessage(MessageType.SYSTEM, Text.of(msg), mc.player.getUuid());
-        } else {
-            for (Map.Entry<String, Double> index : mats.entrySet()) {
-                int numOfMats = (int) Math.round(index.getValue() * amount);
+        ClientCommandManager.DISPATCHER.register(
+                literal("craftlist")
+                        .then(argument("item", string())
+                                .then(argument("amount", integer())
+                                        .executes(ctx -> {
+                                            String item = getString(ctx, "item");
+                                            int amount = getInteger(ctx, "amount");
+                                            Map<String, Double> mats = (Map<String, Double>) materialsPerItem.get(item);
+                                            if (mats == null) {
+                                                String msg = "recipe not found";
+                                                mc.inGameHud.addChatMessage(MessageType.SYSTEM, Text.of(msg), mc.player.getUuid());
+                                            } else {
+                                                for (Map.Entry<String, Double> index : mats.entrySet()) {
+                                                    int numOfMats = (int) Math.round(index.getValue() * amount);
 
-                int stacks = numOfMats / 64;
-                int remainder = (numOfMats % 64);
-                String msg = index.getKey() + " : " + "(" + String.valueOf(numOfMats) + ") " + String.valueOf(stacks) + " stack(s) and " + String.valueOf(remainder) + " items";
-                mc.inGameHud.addChatMessage(MessageType.SYSTEM, Text.of(msg), mc.player.getUuid());
-            }
-        }
+                                                    int stacks = numOfMats / 64;
+                                                    int remainder = (numOfMats % 64);
+                                                    String msg = index.getKey() + " : " + "(" + String.valueOf(numOfMats) + ") " + String.valueOf(stacks) + " stack(s) and " + String.valueOf(remainder) + " items";
+                                                    mc.inGameHud.addChatMessage(MessageType.SYSTEM, Text.of(msg), mc.player.getUuid());
+                                                }
+                                            }
+                                            return 1;
+                                        }))));
     }
+
 }
